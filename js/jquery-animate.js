0807c2b80b4e58a729ac77518cb512a222e62014
Filipe Melo Silva <filipemelodasilva@gmail.com>
2015-03-10 17:47:34 -0300

    Ajustes para remoção de animação e mudança no botão
    Versãos minificadas e grunt

--- js/jquery-animate.js
+++ js/jquery-animate.js
@@ -1,12 +1,11 @@
-var animatedLink = function(locationLink){
+var animatedLink = function(locationLink) {
 	$('html, body').animate({scrollTop: $('#'+locationLink).offset().top }, 1000);
 	return false;
-}
+};
 
-window.onload = function(){
-	$('a.link-nav').bind('touchstart click', function(){
+window.onload = function() {
+	$('a.link-nav').bind('touchstart click', function() {
 		var locationLink = $(this).attr('href').split('#').pop();
 		return animatedLink(locationLink);
 	});
-}
-   
+};
